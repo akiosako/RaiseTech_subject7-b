48eda95880dab9f78867dcbb1d08587fcc5bbd10
akiosako <vaticano2662@gmail.com>
2022-08-31 14:12:34 +0900

    名前以外にも生年月日を受け取れる様に追加変更

--- src/main/java/com/example/restapisample/profileController.java
+++ src/main/java/com/example/restapisample/profileController.java
@@ -1,3 +1,15 @@
 package com.example.restapisample;
 
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class profileController {
+    public String getProfile(
+            @RequestParam(name = "name") String name,
+            @RequestParam(name = "DOB") String DOB) {
+        return "name:" + name + " " + "DOB" + DOB;
+    }
+
+}
 
