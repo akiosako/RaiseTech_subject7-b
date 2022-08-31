937dc14576b7113f43b2a28e640be09a9025acce
akiosako <vaticano2662@gmail.com>
2022-08-31 17:32:39 +0900

    DOBにコメント追加

--- src/main/java/com/example/restapisample/profileController.java
+++ src/main/java/com/example/restapisample/profileController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class profileController {
     public String getProfile(
             @RequestParam(name = "name") String name,
-            @RequestParam(name = "DOB") String DOB) {
+            @RequestParam(name = "DOB") String DOB) { // DOB : day of birth 生年月日
         return "name:" + name + " " + "DOB" + DOB;
     }
 
