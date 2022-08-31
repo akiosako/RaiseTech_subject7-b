171451ae4cb0952d5a5e27e0f7f630a85c96567a
akiosako <vaticano2662@gmail.com>
2022-08-31 21:54:30 +0900

    クラス名をキャメルケースに変更

--- src/main/java/com/example/restapisample/ProfileController.java
+++ src/main/java/com/example/restapisample/ProfileController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class profileController {
+public class ProfileController {
     @GetMapping("/names")
     public String getProfile(
             @RequestParam(name = "name") String name,
