8e64b1c8c81477d49e2f83fc441ad84fb422048a
akiosako <vaticano2662@gmail.com>
2022-08-31 17:48:35 +0900

    GetMappingアノテーションを付与

--- src/main/java/com/example/restapisample/profileController.java
+++ src/main/java/com/example/restapisample/profileController.java
@@ -1,14 +1,16 @@
 package com.example.restapisample;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class profileController {
+    @GetMapping("/names")
     public String getProfile(
             @RequestParam(name = "name") String name,
             @RequestParam(name = "DOB") String DOB) { // DOB : day of birth 生年月日
-        return "name:" + name + " " + "DOB" + DOB;
+        return "name:" + name + " " + "DOB:" + DOB;
     }
 
 }
